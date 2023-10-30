2b457108674e4ecc09ad296bf6c5b76bfdf9e320
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-10-30 23:32:35 +0100

    love calculator 2.0

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
-
-
-
+  let pass = new Audio("./sounds/soft.mp3")
+  pass.play();
+  
+  display.style.display = "none"
+  displayCompat.style.display = "none"
 
 const checkCompact = () => {
   let myName = yourName.value;
@@ -11,18 +12,21 @@ const checkCompact = () => {
     showErr.innerHTML = `<p class="alert alertdanger text-center fw-bold">inputs must be filled</p>`
   }
   else{
+    display.style.display = "block"
+  displayCompat.style.display = "block"
+
     document.getElementById('yourName').value = ""
     loverName.value = ""
     showErr.innerHTML = ""
     display.innerHTML = ""
     let randomize = Math.random()*100;
     let overAll = Math.round(randomize)
-    display.innerHTML = `<p class="text-light text-center"> Love score for ${myName} and ${loveName} : ${overAll}%</p>`
+    display.innerHTML = `<h5 class="extraLg text-black text-center text-white">${overAll}%</h5>`
 
     if (overAll < 50){
-      displayCompat.innerHTML = `<p class="text-center text-warning"> O0ps, You are not compatible</p>`
+      displayCompat.innerHTML = `<h5 class="text-center text-warning bg-black p-2"> Oops, You are not compatible</h5>`
     }else if (overAll >= 50){
-      displayCompat.innerHTML = `<p class="text-success text-center fw-bold">Congratulations you are compatible</p>`
+      displayCompat.innerHTML = `<h5 class=" text-center fw-bold congrats">Congratulations you are compatible</h5>`
     }
   }
 setTimeout(()=>{
